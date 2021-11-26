ff3ae7d31ac288bebcc943d3becc77892f13b6cb
Alfonso Banda <benbanda07@gmail.com>
2021-11-26 11:47:20 -0600

    Add Api calls

--- src/views/Search/index.js
+++ src/views/Search/index.js
@@ -1,12 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import SearchBox from "./components/SearchBox";
 import "./style.css";
-import data from '../../data/users.json';
 import SearchResults from "./components/SearchResults";
 
 export default function Search() {
   const [isAtTop, setIsAtTop] = useState(false);
   const [results, setResults] = useState([])
+  const [data, setData] = useState([])
+  useEffect(() => {
+    const getUsers = async () => {
+     const response = await fetch("https://jsonplaceholder.typicode.com/users")
+     const data = await response.json()
+     setData(data)
+    }
+    getUsers().catch(null)
+  }, [])
+
+
   const handleCloseSearch = () => {
     setIsAtTop(false)
     setResults([])
